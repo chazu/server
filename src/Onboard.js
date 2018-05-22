bfc325e82d64dc96b81e7e93092970ab5d8e0b09
Nirmal Almara <hello@nirm.al>
2018-05-23 01:04:45 +0530

    onboard signin/signout flow

--- src/Onboard.js
+++ src/Onboard.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
+import { Subscribe } from 'unstated';
+import OnboardContainer from './containers/OnboardContainer.js';
 
 class Onboard extends Component {
   render() {
     return (
-      <div className="onboard">
-        <label className="onboard__label">Sign in</label>
-        <input type="text" className="onboard__name" placeholder="Full name" />
-        <input type="email" className="onboard__email" placeholder="Email address" />
-        <input type="password" className="onboard__password" placeholder="Password" />
-        <button className="button button-primary onboard__submit">Submit</button>
-      </div>
+      <Subscribe to={[OnboardContainer]}>
+        {onboard => (
+          <div className="onboard">
+            {
+              onboard.state.isSignedUp
+              ?
+                <div className="onboard__inner">
+                  <label className="onboard__label">Sign In</label>
+                  <input type="email" className="onboard__email" placeholder="Email address" />
+                  <input type="password" className="onboard__password" placeholder="Password" />
+                  <button className="button button-primary onboard__submit">Submit</button>
+                </div>
+              :
+                <div className="onboard__inner">
+                  <label className="onboard__label">Sign Up</label>
+                  <input type="text" className="onboard__name" placeholder="Full name" />
+                  <input type="email" className="onboard__email" placeholder="Email address" />
+                  <input type="password" className="onboard__password" placeholder="Password" />
+                  <button className="button button-primary onboard__submit">Submit</button>
+                </div>
+            }
+          </div>
+          )
+        }
+      </Subscribe>
     );
   }
 }
--- src/containers/OnboardContainer.js
+++ src/containers/OnboardContainer.js
@@ -3,7 +3,14 @@ import { Container } from 'unstated';
 class OnboardContainer extends Container {
   constructor () {
     super();
-    this.state = { isSignedIn: false };
+    this.state = { 
+      isSignedUp: false,
+      isSignedIn: false
+    };
+  }
+
+  signUp() {
+    this.setState({ isSignedUp: true });
   }
   
   signIn() {
